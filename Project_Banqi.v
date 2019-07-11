16e0754a23dd95e624d7bf4d488ab190a44287b0
HomingL <hongming.li@mail.utoronto.ca>
2019-07-11 18:09:31 -0400

    Add main verilog file

---
+++ Project_Banqi.v
@@ -0,0 +1,27 @@
+
+
+module Project_Banqi();
+
+endmodule
+
+module 
+
+
+
+module initilize_game_board(ram);
+
+
+
+endmodule
+
+
+module control(clk, resetn, go);
+	input clk, resetn, go;
+	
+	
+	
+endmodule
+
+
+module datapath();
+endmodule
